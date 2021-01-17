0462e829bdcc5690eb6aeb7727cc6212d272c770
Carlos Lima <carlos.ti.infor@gmail.com>
2021-01-16 23:31:00 -0300

    ForElse

--- aula37/forElse.py
+++ aula37/forElse.py
@@ -4,5 +4,10 @@
 
 variavel = ['Luiz Otávio', 'Joãozinho', 'Maria']
 
+# Checando dados em listas
 for valor in variavel:
-    print(valor)
+    if valor.startswith('J'):
+        print('Comeca com J', valor)
+    else:
+        print('Não comeća com J', valor)
+
