e4a68914d95df316b4dec0346abdd38f503b2d42
Carlos Lima <carlos.ti.infor@gmail.com>
2021-01-16 23:44:03 -0300

    ForElse2

--- aula37/forElse.py
+++ aula37/forElse.py
@@ -4,10 +4,16 @@
 
 variavel = ['Luiz Otávio', 'Joãozinho', 'Maria']
 
-# Checando dados em listas
+# # Checando dados em listas
+comeca_com_j = False
 for valor in variavel:
-    if valor.startswith('J'):
-        print('Comeca com J', valor)
+    if valor.startswith('J'):  # Verificando se determinada letra está em uma lista de nomes.
+        comeca_com_j = True
+
+    if comeca_com_j:
+        print('Existe uma palavra que comeca com J.')
     else:
-        print('Não comeća com J', valor)
+        print('Nào existe uma palavra que comeca com J')
+
+
 
