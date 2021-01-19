52f40ac2651da1066bd9357b89e54c17847d8a7c
Carlos Lima <carlos.ti.infor@gmail.com>
2021-01-19 15:54:43 -0300

    enumerate4

--- aula38/enumerate.py
+++ aula38/enumerate.py
@@ -1,5 +1,19 @@
-string = 'O Brasil é penta'
-lista = string.split(' ')
+# string = 'O Brasil é penta'
+# lista = string.split(' ')
 
-for indice, valor in enumerate(lista):
+# for indice, valor in enumerate(lista):
+#     print(indice, valor)
+
+lista = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+]
+
+# for v in lista:
+#     # print(v)
+#     print(v[0], v[1])
+
+for indice, valor in enumerate(lista):  # Desempacotamento de lista
     print(indice, valor)
+
