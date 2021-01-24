b46e50e3b11d7b4a1b6e2d43cdfa9362f3553a42
Carlos Lima <carlos.ti.infor@gmail.com>
2021-01-23 23:03:20 -0300

    exer

---
+++ aula43/exerc.py
@@ -0,0 +1,15 @@
+# cont = 0
+# con2 = 0
+# while cont < 10:
+#     print(cont)
+#
+#     if cont == 10:
+#         break
+#
+#     cont += 1
+i = 0
+j = 0
+for j, i in range(1, 10):
+    print(j, i)
+
+
